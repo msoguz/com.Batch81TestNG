431e3888f5b29683e3fc8f313c5271bbd8c2696f
M. Sener OGUZ <mseneroguz@gmail.com>
2022-10-20 00:21:21 +0300

    2

--- src/test/java/tests/ListenersTest1.java
+++ src/test/java/tests/ListenersTest1.java
@@ -1,5 +1,33 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+//TEST CLASS
+//Test class'i Utility classa baglamak icin @Listeners annotation'a baglama icin kullanilir
+@Listeners(utilities.Listeners.class)
+
 public class ListenersTest1 {
 
+    @Test
+    public void test1(){
+        System.out.println("Test case 1 - PASS");
+        Assert.assertTrue(true); //pass
+
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test case 2 - FAILED");
+        Assert.assertTrue(false);//fail
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("Test case 3 -SKIPPED");
+        throw new SkipException("Skip Edilecek"); //skip
+
+    }
 }
